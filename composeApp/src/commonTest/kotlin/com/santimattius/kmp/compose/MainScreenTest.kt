00618d8f0bb817d6ccb56bf7c390ad0c8886c63c
Santiago Mattiauda <santimattius@gmail.com>
2024-10-02 20:45:03 -0300

    [started] add tags

--- composeApp/src/commonTest/kotlin/com/santimattius/kmp/compose/MainScreenTest.kt
+++ composeApp/src/commonTest/kotlin/com/santimattius/kmp/compose/MainScreenTest.kt
@@ -1,7 +1,9 @@
 package com.santimattius.kmp.compose
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.runComposeUiTest
@@ -20,5 +22,22 @@ class MainScreenTest {
         onNodeWithContentDescription("Increment").performClick()
         onNodeWithText("1").assertExists()
 
+    }
+
+    @Test
+    fun myFirstTestWithTags() = runComposeUiTest {
+        setContent {
+            MainScreen()
+        }
+        onNodeWithTag(MainScreenTags.HINT_TEXT).assertTextEquals(
+            "You have pushed the button this many times"
+        )
+        onNodeWithTag(MainScreenTags.COUNTER_TEXT).assertTextEquals(
+            "0"
+        )
+        onNodeWithTag(MainScreenTags.INCREMENT_BUTTON).performClick()
+        onNodeWithTag(MainScreenTags.COUNTER_TEXT).assertTextEquals(
+            "1"
+        )
     }
 }
